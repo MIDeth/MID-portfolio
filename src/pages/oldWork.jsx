132f38b91956c9ff12ea363ead01058943adc60d
KVARA7 <abdeljalil0aeb@gmail.com>
2023-10-11 18:09:10 +0100

    main

--- src/pages/oldWork.jsx
+++ src/pages/oldWork.jsx
@@ -1,8 +1,26 @@
 export default function OldWork() {
   return (
     <>
-      <div className="w-[35rem] flex flex-col font-main">
-        <div className="w-[30rem]"></div>
+      <div className="w-[35rem] flex font-main">
+        <div className="w-[14rem] border-2 h-[12rem] rounded-[24px] flex flex-col mx-4">
+          <div className="flex items-center justify-center w-full h-4/6">
+            <img
+              src="work/celer.network.png"
+              className="h-full rounded-t-[21px] w-full"
+            />
+          </div>
+          <div className="w-full border-2 h-2/6 ">n</div>
+        </div>
+
+        <div className="w-[14rem] border-2 h-[12rem] rounded-[24px] flex flex-col mx-4">
+          <div className="flex items-center justify-center w-full h-4/6">
+            <img
+              src="work/veleno.png"
+              className="h-full rounded-t-[21px] w-full"
+            />
+          </div>
+          <div className="w-full border-2 h-2/6 ">n</div>
+        </div>
       </div>
     </>
   );

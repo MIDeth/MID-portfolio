084971c3121e73e223a15af7fe667625d26afe05
KVARA7 <abdeljalil0aeb@gmail.com>
2023-10-11 17:12:24 +0100

    main

--- src/pages/oldWork.jsx
+++ src/pages/oldWork.jsx
@@ -1,8 +1,8 @@
 export default function OldWork() {
   return (
     <>
-      <div className="w-[35rem] font-main">
-        <div></div>
+      <div className="w-[35rem] flex flex-col font-main">
+        <div className="w-[30rem]"></div>
       </div>
     </>
   );

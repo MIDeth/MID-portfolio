feab45c24ab9ef8e366afbf92deaef8579fb7788
KVARA7 <abdeljalil0aeb@gmail.com>
2023-10-11 18:13:44 +0100

    Update oldWork.jsx

--- src/pages/oldWork.jsx
+++ src/pages/oldWork.jsx
@@ -2,24 +2,24 @@ export default function OldWork() {
   return (
     <>
       <div className="w-[35rem] flex font-main">
-        <div className="w-[14rem] border-2 h-[12rem] rounded-[24px] flex flex-col mx-4">
+        <div className="w-[14rem] border-2 h-[12rem] rounded-[14.5px] flex flex-col mx-4">
           <div className="flex items-center justify-center w-full h-4/6">
             <img
               src="work/celer.network.png"
-              className="h-full rounded-t-[21px] w-full"
+              className="h-full rounded-t-[14.5px] w-full"
             />
           </div>
-          <div className="w-full border-2 h-2/6 ">n</div>
+          <div className="w-full h-2/6 "></div>
         </div>
 
-        <div className="w-[14rem] border-2 h-[12rem] rounded-[24px] flex flex-col mx-4">
+        <div className="w-[14rem] border-2 h-[12rem] rounded-[14.5px] flex flex-col mx-4">
           <div className="flex items-center justify-center w-full h-4/6">
             <img
               src="work/veleno.png"
-              className="h-full rounded-t-[21px] w-full"
+              className="h-full rounded-t-[14.5px] w-full"
             />
           </div>
-          <div className="w-full border-2 h-2/6 ">n</div>
+          <div className="w-full h-2/6 ">n</div>
         </div>
       </div>
     </>

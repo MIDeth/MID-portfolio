f8f5747c129e9c9c0c3113c7e6a16aec53485dcd
KVARA7 <abdeljalil0aeb@gmail.com>
2023-10-12 22:46:29 +0100

    add instrumental to background 🌙

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,53 @@
+import { Routes, Route } from "react-router-dom";
+import { Component } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import About from "./pages/about";
 import OldWork from "./pages/oldWork";
-import { Routes, Route } from "react-router-dom";
+import Loading from "./pages/loading";
 
-function App() {
-  return (
-    <>
-      <div className="flex justify-center mt-20">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/oldWork" element={<OldWork />} />
-        </Routes>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true, // Initially, set loading to true
+    };
+  }
+
+  componentDidMount() {
+    // Simulate loading dependencies (you can replace this with your actual logic)
+    setTimeout(() => {
+      // Once the dependencies are loaded, set loading to false
+      this.setState({ loading: false });
+    }, 3000); // Replace 3000 with the actual loading time
+
+    // You can load your dependencies (e.g., data, API calls) here
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.loading ? (
+          <Loading />
+        ) : (
+          <>
+            <div className="flex justify-center mt-20">
+              <Header />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/oldWork" element={<OldWork />} />
+              </Routes>
+            </div>
+            <audio controls autoPlay loop>
+              <source src="/public/audio/background.mp4" type="audio/mp4" />
+            </audio>
+          </>
+        )}
       </div>
-    </>
-  );
+    );
+  }
 }
 
 export default App;
